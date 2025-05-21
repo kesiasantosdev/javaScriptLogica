0bbe62da831a189d935b02f1f8cb74c1695a2736
Kesia dos Santos <kesiasantos.dev@gmail.com>
2025-05-20 23:26:40 -0300

    Add files via upload

---
+++ JavaScript/Figuras geometricas/Quadrado/script.js
@@ -0,0 +1,4 @@
+alert("Programa para calcular a área do Quadrado");
+let lado = parseFloat(prompt("Digite o valor do lado:"));
+let area = lado * lado;
+alert("O valor da área é " + area + "cm");
---
+++ JavaScript/Operações Aritmeticas/Subtração/script.js
@@ -0,0 +1,4 @@
+let numero = parseFloat(prompt("Digite o primeiro número:"));
+let numero01 = parseFloat(prompt("Digite o segundo número:"));
+let subtracao = numero - numero01;
+alert("O valor da subtração é " + subtracao)
