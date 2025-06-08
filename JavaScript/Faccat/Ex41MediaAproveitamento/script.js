d454134fb2cf32fdfd84e88b3774e9455bae81d9
Kesia dos Santos <kesiasantos.dev@gmail.com>
2025-06-08 13:23:28 -0300

    Update script.js

--- JavaScript/Faccat/Ex41MediaAproveitamento/script.js
+++ JavaScript/Faccat/Ex41MediaAproveitamento/script.js
@@ -3,8 +3,8 @@ alert("Programa que calcula a média de aproveitamento.")
 let N1 = parseFloat(prompt("Digite a nota da 1ª verificação (N1):"));
 let N2 = parseFloat(prompt("Digite a nota da 2ª verificação (N2):"));
 let N3 = parseFloat(prompt("Digite a nota da 3ª verificação (N3):"));
-let mediaExercício = parseFloat(prompt("Digite a média dos exercícios:"));
-let mediaAproveitamento = (N1 + N2 * 2 + N3 * 3 + mediaExercício) / 7;
+let mediaExercicio = parseFloat(prompt("Digite a média dos exercícios:"));
+let mediaAproveitamento = (N1 + N2 * 2 + N3 * 3 + mediaExercicio) / 7;
 let conceito;
 
 if (mediaAproveitamento >= 10.0) {
